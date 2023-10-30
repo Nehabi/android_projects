2d838c5346926c900e2f024e49d468d4814dda55
Nehabi <bneh19@gmail.com>
2023-10-29 18:40:07 -0700

    images

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.diceroller.databinding.ActivityMainBinding
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
@@ -13,5 +14,21 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         binding.rollButton.text = btnText
+        binding.rollButton.setOnClickListener{
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val imgSrc = when(Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+        binding.diceImage.setImageResource(imgSrc)
     }
 }
