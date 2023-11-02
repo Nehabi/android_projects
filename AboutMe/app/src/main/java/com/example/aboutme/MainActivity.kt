3f18143c9599cb1d34f6487c43baf74a26222f96
Nehabi <bneh19@gmail.com>
2023-11-02 00:05:43 -0700

    data binding changes

--- AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -8,23 +8,27 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.done_button).setOnClickListener {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.doneButton.setOnClickListener {
             addNickName(it)
         }
     }
 
     private fun addNickName(view: View) {
-        val editText = findViewById<EditText>(R.id.editText)
-        val nickNameTextView = findViewById<TextView>(R.id.text)
-        nickNameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nickNameTextView.visibility = View.VISIBLE
+        binding.apply {
+            text.text = editText.text
+            editText.visibility = View.GONE
+            view.visibility = View.GONE
+            text.visibility = View.VISIBLE
+        }
 
         //hide the keyboard
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
