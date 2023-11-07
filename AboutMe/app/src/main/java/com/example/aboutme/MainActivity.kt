0d06501d0a91b0b25e198eed4b20cdab23d5df33
Nehabi <bneh19@gmail.com>
2023-11-07 12:28:47 +0530

    About me final changes

--- AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -12,11 +12,13 @@ import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
+    private val myName : MyName = MyName("Neha Bisht")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        binding.myName = myName
         binding.doneButton.setOnClickListener {
             addNickName(it)
         }
@@ -24,10 +26,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun addNickName(view: View) {
         binding.apply {
-            text.text = editText.text
-            editText.visibility = View.GONE
+            binding.myName?.nickName = nickNameEditText.text.toString()
+            invalidateAll()
+            nickNameEditText.visibility = View.GONE
             view.visibility = View.GONE
-            text.visibility = View.VISIBLE
+            nickName.visibility = View.VISIBLE
         }
 
         //hide the keyboard
